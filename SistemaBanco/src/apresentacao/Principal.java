40068d7c62391d63042490dac1b5d780f2008ea0
Gustavo Braga <117944429+GustBraga@users.noreply.github.com>
2023-03-16 14:17:40 -0300

    Update Principal.java

--- SistemaBanco/src/apresentacao/Principal.java
+++ SistemaBanco/src/apresentacao/Principal.java
@@ -3,6 +3,7 @@ package apresentacao;
 import java.util.Scanner;
 
 import entidade.ContaCC;
+import entidade.ContaCP;
 
 public class Principal {
 
@@ -61,8 +62,8 @@ public class Principal {
 							} else {
 								System.out.print("Digite o valor do seu Limite: ");
 								double limite = Double.parseDouble(ler.nextLine());
-								if (limite < 0 || new ContaCC().verificaDigito(limite) == false || Double.toString(saldo).charAt(0) == '.') {
-									System.out.println("Valor de Saldo inválido!");
+								if (limite < 0 || new ContaCC().verificaDigito(limite) == false || Double.toString(limite).charAt(0) == '.') {
+									System.out.println("Valor de Limite inválido!");
 									break;
 								} else {
 									System.out.print("Digite o seu Cpf(Ex: 115.977.897-39): ");
@@ -86,8 +87,55 @@ public class Principal {
 			}
 
 			case "2": {
+				
+				System.out.print("Digite o Número da sua Conta(Ex: 552018-0): ");
+				String nrConta = ler.nextLine().trim();
+				if (nrConta.isEmpty() || new ContaCP().verificaNrConta(nrConta) == false) {
+					System.out.println("Número de conta inválido!");
+					break;
+				} else {
+					System.out.print("Digite a sua Agência(Ex: 4605): ");
+					String agencia = ler.nextLine().trim();
+					if (agencia.isEmpty() || new ContaCP().verificaAgencia(agencia) == false) {
+						System.out.println("Agência inválida! ");
+						break;
+					} else {
+						System.out.print("Digite o nome do Titular: ");
+						String titular = ler.nextLine().trim().replace("  ", " ");
+						if (titular.isEmpty() || new ContaCP().verificaTitular(titular) == false) {
+							System.out.println("Nome do Titular inválido!");
+							break;
+						} else {
+							System.out.print("Digite o valor do seu Saldo: ");
+							double saldo = Double.parseDouble(ler.nextLine());
+							if (saldo < 0 || new ContaCP().verificaDigito(saldo) == false || Double.toString(saldo).charAt(0) == '.') {
+								System.out.println("Valor de Saldo inválido!");
+								break;
+							} else {
+								System.out.print("Digite o valor do seu Rendimento: ");
+								double rendimento = Double.parseDouble(ler.nextLine());
+								if (rendimento < 0 || new ContaCP().verificaDigito(rendimento) == false || Double.toString(rendimento).charAt(0) == '.') {
+									System.out.println("Valor de Rendimento inválido!");
+									break;
+								} else {
+									System.out.print("Digite o seu Cpf(Ex: 115.977.897-39): ");
+									String cpf = ler.nextLine().trim();
+									if (cpf.isEmpty() || new ContaCP().verificaCpf(cpf) == false) {
+										System.out.println("Cpf inválido!");
+										break;
+									} else {
+										
+										ContaCP cp = new ContaCP(nrConta, agencia, titular, saldo, rendimento, cpf);
+										
+										System.out.println(cp.cadastrarCP());
 
-				break;
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 
 			case "3": {
@@ -105,6 +153,15 @@ public class Principal {
 			}
 
 			case "4": {
+				
+				System.out.print("Digite o seu Cpf para exibir dados(Ex: 115.977.897-39): ");
+				String cpf = ler.nextLine().trim();
+				if (cpf.isEmpty() || new ContaCP().verificaCpf(cpf) == false) {
+					System.out.println("Cpf inválido!");
+					break;
+				}
+
+				System.out.println(new ContaCP().verificarCP(cpf));
 
 				break;
 			}
@@ -117,7 +174,7 @@ public class Principal {
 					System.out.println("Valor de Deposito inválido!");
 					break;
 				}
-				System.out.print("Digite o Cpf paara Deposito(Ex: 115.977.897-39): ");
+				System.out.print("Digite o Cpf para Deposito(Ex: 115.977.897-39): ");
 				String cpf = ler.nextLine();
 				if (cpf.isEmpty() || new ContaCC().verificaCpf(cpf) == false) {
 					System.out.println("Cpf inválido!");
@@ -130,6 +187,21 @@ public class Principal {
 			}
 
 			case "6": {
+				
+				System.out.print("Digite o valor do Deposito: ");
+				double valorDeposito = Double.parseDouble(ler.nextLine().trim());
+				if (valorDeposito < 0) {
+					System.out.println("Valor de Deposito inválido!");
+					break;
+				}
+				System.out.print("Digite o Cpf para Deposito(Ex: 115.977.897-39): ");
+				String cpf = ler.nextLine();
+				if (cpf.isEmpty() || new ContaCP().verificaCpf(cpf) == false) {
+					System.out.println("Cpf inválido!");
+					break;
+				}
+
+				System.out.println(new ContaCP().depositarCP(valorDeposito, cpf));
 
 				break;
 			}
@@ -142,7 +214,7 @@ public class Principal {
 					System.out.println("Valor de Saque inválido!");
 					break;
 				}
-				System.out.print("Digite o Cpf paara Deposito(Ex: 115.977.897-39): ");
+				System.out.print("Digite o Cpf para Deposito(Ex: 115.977.897-39): ");
 				String cpf = ler.nextLine();
 				if (cpf.isEmpty() || new ContaCC().verificaCpf(cpf) == false) {
 					System.out.println("Cpf inválido!");
@@ -155,6 +227,21 @@ public class Principal {
 			}
 
 			case "8": {
+				
+				System.out.print("Digite o valor do seu Saque: ");
+				double valorSaque = Double.parseDouble(ler.nextLine().trim());
+				if (valorSaque < 0) {
+					System.out.println("Valor de Saque inválido!");
+					break;
+				}
+				System.out.print("Digite o Cpf para Deposito(Ex: 115.977.897-39): ");
+				String cpf = ler.nextLine();
+				if (cpf.isEmpty() || new ContaCP().verificaCpf(cpf) == false) {
+					System.out.println("Cpf inválido!");
+					break;
+				}
+
+				System.out.println(new ContaCP().sacarCP(valorSaque, cpf));
 
 				break;
 			}
@@ -167,6 +254,8 @@ public class Principal {
 			}
 
 			case "10": {
+				
+				System.out.println(new ContaCP().listarContasCP());
 
 				break;
 			}
